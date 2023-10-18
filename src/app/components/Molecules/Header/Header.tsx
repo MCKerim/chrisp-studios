3784e1eaf6ddae0af75e567dcd0be76b39c938b6
Kerim <MCKerim@gmx.de>
2023-10-18 09:27:22 +0000

    Navbar now changes on screen size

--- src/app/components/Molecules/Header/Header.tsx
+++ src/app/components/Molecules/Header/Header.tsx
@@ -1,6 +1,30 @@
 import Link from "next/link";
 
+type sectionDetails = {
+  lable: string;
+  link: string;
+};
+
 export default function Header() {
+  const sections: sectionDetails[] = [
+    {
+      lable: "HOME",
+      link: "#section-home",
+    },
+    {
+      lable: "ABOUT",
+      link: "#section-about",
+    },
+    {
+      lable: "PROJECTS",
+      link: "#section-projects",
+    },
+    {
+      lable: "CONTACT",
+      link: "#section-socialMedia",
+    },
+  ];
+
   function renderHeaderLink(label: string, link: string) {
     return (
       <Link href={link} className="group">
@@ -28,63 +52,66 @@ export default function Header() {
   }
 
   return (
-    <div className="navbar sticky top-0 z-40 justify-around">
-      {renderHeaderLink("HOME", "#section-home")}
-      {renderHeaderLink("ABOUT", "#section-about")}
-      <img
-        style={{
-          height: "50px",
-        }}
-        src="/Logo.png"
-        alt=""
-      />
-      {renderHeaderLink("PROJECTS", "#section-projects")}
-      {renderHeaderLink("CONTACT", "#section-socialMedia")}
-    </div>
-  );
-  return (
-    <div className="navbar bg-base-100 sticky top-0 z-40">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost btn-circle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h7"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <Link href="#section-home">Home</Link>
-            </li>
-            <li>
-              <Link href="#section-about">About</Link>
-            </li>
-            <li>
-              <Link href="#section-projects">Projects</Link>
-            </li>
-            <li>
-              <Link href="#section-socialMedia">Social Media</Link>
-            </li>
-          </ul>
+    <>
+      <div className="hidden md:block sticky top-0 z-40">
+        <div className="navbar justify-around">
+          {sections.map((section, index) => (
+            <>
+              {renderHeaderLink(section.lable, section.link)}
+              {index === 1 ? (
+                <img
+                  style={{
+                    height: "50px",
+                  }}
+                  src="/Logo.png"
+                  alt=""
+                />
+              ) : (
+                ""
+              )}
+            </>
+          ))}
         </div>
       </div>
-      <div className="navbar-center">
-        <a className="btn btn-ghost normal-case text-xl">ChrisP Studios</a>
+
+      <div className="md:hidden sticky top-0 z-40">
+        <div className="navbar bg-base-100">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <label tabIndex={0} className="btn btn-ghost btn-circle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h7"
+                  />
+                </svg>
+              </label>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                {sections.map((section) => (
+                  <li key={section.link}>
+                    <Link href={section.link}>{section.lable}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div className="navbar-center">
+            <a className="btn btn-ghost normal-case text-xl">ChrisP Studios</a>
+          </div>
+          <div className="navbar-end"></div>
+        </div>
       </div>
-      <div className="navbar-end"></div>
-    </div>
+    </>
   );
 }
